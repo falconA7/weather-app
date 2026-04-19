2c5a7fc38ea510335e956b2fd52a09ff6ec45a25
shunta aoki <kaweco17@gmail.com>
2026-04-19 22:03:03 +0900

    add some files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Title from "./components/Title"
+import Form from "./components/Form"
+
+const App =()=>{
+    return(
+      <div>
+        <Title/>
+        <Form/>
+      </div>
+    )
+}
+
+export default App 
