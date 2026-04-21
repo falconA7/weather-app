8539bbd7c36c00b0ce9ccf6b90eb98e5194c6444
shunta aoki <kaweco17@gmail.com>
2026-04-21 23:10:20 +0900

    fixed Form.tsx file and app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import {useState} from "react"
 import Title from "./components/Title"
 import Form from "./components/Form"
 import Results from "./components/Results"
 
 const App =()=>{
+
+  const [city,setCity] = useState<string>("")
+  
+      const getWeather = (e: any) => {
+          e.preventDefault()
+          fetch(`http://api.weatherapi.com/v1/current.json?key=6c1757eee273440f9c312124262004&q=${city}&aqi=no`
+          )
+              .then(res=> res.json() )
+              .then(data=>console.log(data))
+      }
+  
     return(
       <div>
         <Title/>
-        <Form/>
+        <Form setCity={setCity} getWeather={getWeather}/>
         <Results/>
       </div>
     )
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,4 @@
-import {useState} from "react"
-
 const Form = ()=> {
-    const [city,setCity] = useState<string>("")
-
-    const getWeather = (e: any) => {
-        e.preventDefault()
-        fetch("http://api.weatherapi.com/v1/current.json?key=6c1757eee273440f9c312124262004&q=${city}&aqi=no"
-        )
-            .then(res=> res.json() )
-            .then(data=>console.log(data))
-    }
-
     return (
         <form >
             <input type="text" 
@@ -25,5 +13,6 @@ const Form = ()=> {
         </form>
     )
 }
+
 
 export default Form
