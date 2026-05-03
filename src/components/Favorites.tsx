1218e9b41ad3230a730aa6fe0a1a01a99585eb2b
shunta aoki <kaweco17@gmail.com>
2026-05-03 13:46:20 +0900

    fixed index.css&favorites.tsx files index.css:changed form design favorites.tsx: fovorite city button

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -3,6 +3,11 @@ const Favorites = ()=>{
     return(
         <div className="favorites">
             <h2>Favorite palaces</h2>
+            <div>
+                <button onClick={}>London</button>
+                <button onClick={}>Tokyo</button>
+                <button onClick={}>NEW-YORK</button>
+            </div>
         </div>
     )
 }
