77de8a672e09f37fe11a3f972fb094ecfa564ec9
shunta aoki <kaweco17@gmail.com>
2026-05-04 01:14:11 +0900

    fixed Favorites.tsx {add onClick method}

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,12 +1,15 @@
+type{
 
-const Favorites = ()=>{
+}
+
+const Favorites = (props:favoritesProps)=>{
     return(
         <div className="favorites">
             <h2>Favorite palaces</h2>
             <div>
-                <button onClick={}>London</button>
-                <button onClick={}>Tokyo</button>
-                <button onClick={}>NEW-YORK</button>
+                <button onClick={props.getWeather}>London</button>
+                <button onClick={props.getWeather}>Tokyo</button>
+                <button onClick={props.getWeather}>NEW-YORK</button>
             </div>
         </div>
     )
